577f59d03c0dcde36bfc44a03a6584faa0bc6992
Angel <angeldariaux@gmail.com>
2024-03-25 20:22:08 -0600

    added validations to the models file

--- models/models.py
+++ models/models.py
@@ -1,9 +1,9 @@
 # File where the APi's models will be store 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class Product(BaseModel):
-  id: int
-  name: str
-  description: str
-  price: float
+  id: int = Field(..., gt=0, description="The unique identifier for the product")
+  name: str = Field(..., min_length=1, max_length=50, description="The name of the product")
+  description: str = Field(..., min_length=1, max_length=150, description="The description of the product")
+  price: float = Field(..., gt=0, description="The price of the product must be greater than 0")
   
